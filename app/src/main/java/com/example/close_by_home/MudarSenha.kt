b5212e7b62ff0a02c6326c916d64c17aa910e011
Felipe <felipe.andrade@bandtec.com.br>
2023-04-10 19:30:26 -0300

    Tela login e mudar senha finalizadas

---
+++ app/src/main/java/com/example/close_by_home/MudarSenha.kt
@@ -0,0 +1,11 @@
+package com.example.close_by_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MudarSenha : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mudar_senha)
+    }
+}
