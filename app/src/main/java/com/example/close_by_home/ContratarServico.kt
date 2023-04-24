155b80902c9792f749c4a198f4baffad4aa225aa
Felipe <felipe.andrade@bandtec.com.br>
2023-04-24 18:52:34 -0300

    adicionando layout home

---
+++ app/src/main/java/com/example/close_by_home/ContratarServico.kt
@@ -0,0 +1,11 @@
+package com.example.close_by_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContratarServico : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contratar_servico)
+    }
+}
